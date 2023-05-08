1d127c4efacaec3b5c5bc1cfbe0c9705d98b6f33
quiqueferre12 <61469172+quiqueferre12@users.noreply.github.com>
2023-05-09 01:33:45 +0200

    ruta por puntos habitaciones
    ruta por las habitaciones my_waypoint_folllower mediante puntos.

--- proy_techcommit_my_nav2_system/proy_techcommit_my_nav2_system/my_waypoint_follower.py
+++ proy_techcommit_my_nav2_system/proy_techcommit_my_nav2_system/my_waypoint_follower.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
+import time
 
 #from nav2_msgs.action import NavigateToPose
 
@@ -22,51 +23,72 @@ class WaypointFollower(Node):
         # Definir una lista de waypoints como objetos de tipo PoseStamped
         waypoints = []
         # Agregar waypoints a la lista en el formato deseado
-                # Waypoint 1
-        waypoint1 = PoseStamped()
-        waypoint1.header.frame_id = 'map'
-        waypoint1.pose.position.x = 1.0
-        waypoint1.pose.position.y = 2.0
-        waypoint1.pose.orientation.z = 0.0
-        waypoint1.pose.orientation.w = 1.0
-        waypoints.append(waypoint1)
-
-    # Waypoint 2
+        # Waypoint 2
         waypoint2 = PoseStamped()
         waypoint2.header.frame_id = 'map'
-        waypoint2.pose.position.x = 3.0
-        waypoint2.pose.position.y = 4.0
+        waypoint2.pose.position.x = -9.6
+        waypoint2.pose.position.y = 3.71
         waypoint2.pose.orientation.z = 0.0
         waypoint2.pose.orientation.w = 1.0
         waypoints.append(waypoint2)
-
-    # Waypoint 3
+        # Waypoint 3
         waypoint3 = PoseStamped()
         waypoint3.header.frame_id = 'map'
-        waypoint3.pose.position.x = 5.0
-        waypoint3.pose.position.y = 6.0
+        waypoint3.pose.position.x = -19.7
+        waypoint3.pose.position.y = -7.94
         waypoint3.pose.orientation.z = 0.0
         waypoint3.pose.orientation.w = 1.0
         waypoints.append(waypoint3)
-
+        
     # Waypoint 4
         waypoint4 = PoseStamped()
         waypoint4.header.frame_id = 'map'
-        waypoint4.pose.position.x = 7.0
-        waypoint4.pose.position.y = 8.0
+        waypoint4.pose.position.x = -17.5
+        waypoint4.pose.position.y = -3.54
         waypoint4.pose.orientation.z = 0.0
         waypoint4.pose.orientation.w = 1.0
         waypoints.append(waypoint4)
+        # Waypoint 4_5
+        waypoint4_5 = PoseStamped()
+        waypoint4_5.header.frame_id = 'map'
+        waypoint4_5.pose.position.x = -16.1
+        waypoint4_5.pose.position.y = -7.31
+        waypoint4_5.pose.orientation.z = 0.0
+        waypoint4_5.pose.orientation.w = 1.0
+        waypoints.append(waypoint4_5)
 
     # Waypoint 5
         waypoint5 = PoseStamped()
         waypoint5.header.frame_id = 'map'
-        waypoint5.pose.position.x = 9.0
-        waypoint5.pose.position.y = 10.0
+        waypoint5.pose.position.x = -10.2
+        waypoint5.pose.position.y = -15.6
         waypoint5.pose.orientation.z = 0.0
         waypoint5.pose.orientation.w = 1.0
         waypoints.append(waypoint5)
-
+    # Waypoint 6
+        waypoint6 = PoseStamped()
+        waypoint6.header.frame_id = 'map'
+        waypoint6.pose.position.x = -10.2
+        waypoint6.pose.position.y = -12.7
+        waypoint6.pose.orientation.z = 0.0
+        waypoint6.pose.orientation.w = 1.0
+        waypoints.append(waypoint6)
+        # Waypoint 7
+        waypoint7 = PoseStamped()
+        waypoint7.header.frame_id = 'map'
+        waypoint7.pose.position.x = 1.66
+        waypoint7.pose.position.y = -9.01
+        waypoint7.pose.orientation.z = 0.0
+        waypoint7.pose.orientation.w = 1.0
+        waypoints.append(waypoint7)
+        # Waypoint 8
+        waypoint8 = PoseStamped()
+        waypoint8.header.frame_id = 'map'
+        waypoint8.pose.position.x = 1.28
+        waypoint8.pose.position.y = -11.1
+        waypoint8.pose.orientation.z = 0.0
+        waypoint8.pose.orientation.w = 1.0
+        waypoints.append(waypoint8)
    
         return waypoints
 
@@ -112,7 +134,10 @@ class WaypointFollower(Node):
     #definimos la funcion de respuesta al resultado
     def get_result_callback(self, future):
         result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.status))
+        self.get_logger().info('Result: {0}'.format(result.missed_waypoints))
+        time.sleep(1)
+        # Cerrar el nodo y llamar a rclpy.shutdown() después de recibir el resultado de la acción
+        self.destroy_node()
         rclpy.shutdown()
 
 def main(args=None):
